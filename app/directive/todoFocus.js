e7da41d6ec97a7b1855ad691ba7292729052e473
vinlon <vinlon1009@gmial.com>
2016-07-06 22:27:26 +0800

    modified:   app/controller/todo.js 	modified:   app/directive/todoEscape.js 	modified:   app/view/index.html 	modified:   index.html 	modified:   lib/todo-app/index.css 	modified:   ref/index.html 	modified:   ref/js/directives/todoFocus.js

---
+++ app/directive/todoFocus.js
@@ -0,0 +1,21 @@
+/*global angular */
+
+/**
+ * Directive that places focus on the element it is applied to when the
+ * expression it binds to evaluates to true
+ */
+todo.directive('todoFocus', function todoFocus($timeout) {
+		'use strict';
+        //切换到编辑模式后自动获取焦点
+		return function (scope, elem, attrs) {
+            //delay为0时，focus效果不是很稳定，原因不明
+            var delay = 100;
+			scope.$watch(attrs.todoFocus, function (newVal) {
+                if(newVal){
+                    $timeout(function () {
+						elem[0].focus();
+					}, delay, false);
+                }
+			});
+		};
+	});
